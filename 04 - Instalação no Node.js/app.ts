56c73a5701b81a802f3bf94686e783abd9c46c6b
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-06 14:08:27 -0300

    config ambiente

---
+++ 04 - Instalação no Node.js/app.ts
@@ -0,0 +1,9 @@
+let nome: string;
+let sobrenome: string;
+let idade: number;
+
+nome = 'Luis';
+sobrenome = 'Silva';
+idade = 21;
+
+console.log(`nome: ${nome}, sobrenome: ${sobrenome}, idade: ${idade}`);
