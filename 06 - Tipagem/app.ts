4fb874613846b188274c4566b0384f3af2cd9f9e
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-06 14:37:41 -0300

    tipagem: variáveis

---
+++ 06 - Tipagem/app.ts
@@ -0,0 +1,16 @@
+// let nome: string;
+// let idade: number;
+// let maiorIdade: boolean;
+
+let nome = 'Luis';
+let idade = 21;
+let maiorIdade = true;
+
+let a = 5;
+let b = 7;
+
+function soma(a: number, b: number) {
+    return a + b;
+}
+
+console.log(`Soma: ${soma(a, b)}`);
